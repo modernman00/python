8e942e260e85930906fea2206fe08ebf6599a381
modernman00 <43863051+modernman00@users.noreply.github.com>
2019-09-25 01:50:30 +0100

    Add files via upload

---
+++ employVariable.py
@@ -0,0 +1,10 @@
+a = b = c = 8
+
+print(b)
+
+a, b, c = 1, 2, 3
+
+print(c)
+
+user = input('What is your name')
+print('Welcome ', user)
---
+++ first.py
@@ -0,0 +1,9 @@
+a=b=c=8
+
+print(b)
+
+a,b,c = 1, 2, 3
+
+print(c)
+
+
